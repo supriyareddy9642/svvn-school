aa036f691a79792dc22e7ba552369d9cc579eef9
supriyareddy9642 <supriyareddy9642@gmail.com>
2026-04-29 15:35:00 +0530

    Update Navbar.jsx

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -5,22 +5,20 @@ import logo from "../assets/logo.jpg";
 
 export default function CustomNavbar() {
   return (
-    <Navbar expand="lg" className="navbar">
+    <Navbar expand="lg" className="custom-navbar">
       <Container>
-        
+
         {/* Logo */}
         <Navbar.Brand as={Link} to="/">
           <img src={logo} alt="logo" className="my-logo" />
         </Navbar.Brand>
-         <Link to="/apply" className="apply-btn ms-auto d-block d-md-none">
-            Call Now
-          </Link>
+
         {/* Toggle */}
         <Navbar.Toggle aria-controls="navbarNav" />
 
         {/* Menu */}
         <Navbar.Collapse id="navbarNav">
-          <Nav className="navbar-nav">
+          <Nav className="navbar-nav mx-auto">
 
             <Nav.Link as={Link} to="/">Home</Nav.Link>
             <Nav.Link as={Link} to="/about">About</Nav.Link>
@@ -32,9 +30,14 @@ export default function CustomNavbar() {
 
           </Nav>
 
-          {/* Apply Button */}
-          <Link to="/apply" className="apply-btn ms-auto d-none d-md-block">
+          {/* Desktop Button */}
+          <Link to="/apply" className="apply-btn d-none d-md-block">
             Apply Now
+          </Link>
+
+          {/* Mobile Button */}
+          <Link to="/apply" className="apply-btn d-md-none">
+            Call Now
           </Link>
 
         </Navbar.Collapse>
