ae32680576e8178d8d1f2f19ec12e2c471cdaf77
supriyareddy9642 <supriyareddy9642@gmail.com>
2026-04-29 17:15:16 +0530

    Update Topbar.jsx

--- src/components/Topbar.jsx
+++ src/components/Topbar.jsx
@@ -2,21 +2,39 @@ import { Container } from "react-bootstrap";
 import { FaEnvelope, FaUniversity, FaUserGraduate, FaCalendarAlt, FaBriefcase, FaPhoneAlt } from "react-icons/fa";
 import { Link } from "react-router-dom";
 import '../styles/Topbar.css';
-<div className="topbar">
-  <Container className="d-flex justify-content-between align-items-center">
-    
-    <div className="left-info">
-      <span><FaEnvelope /> contact@myschool.com</span>
-      <span className="line">|</span>
-      <span><FaPhoneAlt /> +91 95506 61987</span>
-    </div>
 
-    <div className="right-links">
-      <Link to="/campus">Campus</Link>
-      <Link to="/students">Students</Link>
-      <Link to="/events">Events</Link>
-      <Link to="/careers">Careers</Link>
-    </div>
+export default function Topbar() {
+  return (
+    <div className="topbar d-none d-md-block">
+      <Container>
+        <div className="topbar-inner">
+          
+          <div className="left-info">
+            <span>
+              <FaEnvelope /> contact@myschool.com
+            </span>
+            <span className="line">|</span>
 
-  </Container>
-</div>
+            <span>
+              <FaPhoneAlt /> +91 95506 61987
+            </span>
+          </div>
+
+          <div className="right-links">
+            <Link to="/campus"><FaUniversity /> Campus</Link>
+            <span className="line">|</span>
+
+            <Link to="/students"><FaUserGraduate /> Students</Link>
+            <span className="line">|</span>
+
+            <Link to="/events"><FaCalendarAlt /> Events</Link>
+            <span className="line">|</span>
+
+            <Link to="/careers"><FaBriefcase /> Careers</Link>
+          </div>
+
+        </div>
+      </Container>
+    </div>
+  );
+}
