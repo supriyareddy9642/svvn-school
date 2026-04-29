65a63c69d015e30857020ee34d33235d73dbbe25
supriyareddy9642 <supriyareddy9642@gmail.com>
2026-04-29 15:13:13 +0530

    Update Topbar.jsx

--- src/components/Topbar.jsx
+++ src/components/Topbar.jsx
@@ -1,45 +1,39 @@
 import { Container } from "react-bootstrap";
-import { FaEnvelope, FaPhoneVolume, FaUniversity, FaUserGraduate, FaCalendarAlt, FaBriefcase, FaPhone, FaPhoneAlt } from "react-icons/fa";
+import { FaEnvelope, FaUniversity, FaUserGraduate, FaCalendarAlt, FaBriefcase, FaPhoneAlt } from "react-icons/fa";
 import { Link } from "react-router-dom";
 import '../styles/Topbar.css';
 
 export default function Topbar() {
   return (
     <div className="topbar d-none d-md-block">
-      <Container className="d-flex justify-content-between">
-        
-        <div className="left-info">
-          <span>
-            <FaEnvelope /> contact@myschool.com
-          </span>
-          <span className="line">&nbsp;&nbsp; | &nbsp;&nbsp;</span>
+      <Container>
+        <div className="topbar-inner">
+          
+          <div className="left-info">
+            <span>
+              <FaEnvelope /> contact@myschool.com
+            </span>
+            <span className="line">|</span>
 
-          <span>
-            <FaPhoneAlt /> +91 95506 61987
-          </span>
+            <span>
+              <FaPhoneAlt /> +91 95506 61987
+            </span>
+          </div>
+
+          <div className="right-links">
+            <Link to="/campus"><FaUniversity /> Campus</Link>
+            <span className="line">|</span>
+
+            <Link to="/students"><FaUserGraduate /> Students</Link>
+            <span className="line">|</span>
+
+            <Link to="/events"><FaCalendarAlt /> Events</Link>
+            <span className="line">|</span>
+
+            <Link to="/careers"><FaBriefcase /> Careers</Link>
+          </div>
+
         </div>
-
-        <div className="right-links">
-          <Link to="/campus">
-            <FaUniversity /> Campus
-          </Link>
-          <span className="line">|</span>
-
-          <Link to="/students">
-            <FaUserGraduate /> Students
-          </Link>
-          <span className="line">|</span>
-
-          <Link to="/events">
-            <FaCalendarAlt /> Events
-          </Link>
-          <span className="line">|</span>
-
-          <Link to="/careers">
-            <FaBriefcase /> Careers
-          </Link>
-        </div>
-
       </Container>
     </div>
   );
